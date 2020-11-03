ad677986336afa902c37ee703a5a2419e3e75b1f
cfor0 <cfor0@outlook.com>
2020-11-03 09:49:42 -0500

    trying arrow functions

--- veryEasy/script.js
+++ veryEasy/script.js
@@ -1,9 +1,17 @@
-function min(a,b) {
-    if (a > b) {
-        console.log(`${b} is the minimum.`)
-    } else {
-        console.log(`${a} is the minimum.`)
-    }
+// function min(a,b) {
+//     if (a > b) {
+//         console.log(`${b} is the minimum.`)
+//     } else {
+//         console.log(`${a} is the minimum.`)
+//     }
+// }
+
+// min(345, 987)
+
+
+const minArrow = (a, b) => {
+    const minNum = (a < b) ? `${a} is the minimum.`: `${b} is the minimum.`;
+    return minNum;
 }
 
-min(345, 987)
+console.log(minArrow(34,31))
